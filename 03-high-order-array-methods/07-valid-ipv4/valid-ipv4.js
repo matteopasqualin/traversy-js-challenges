e9982f2c8e34080e6200be5ba0b1efaca5077187
matteopasqualin <matteo.pasqualin@prominder.eu>
2026-01-19 17:38:53 +0100

    ok fino a adesso

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,21 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (text) => {
+  //return (text.split('.').length != 4)
+  //  ? false
+  //  : text.split('.').every(item => (parseInt(item) === NaN) ? false : parseInt(item) >= 0 && parseInt(item) <= 255);
+
+  return text.split(".").length != 4
+    ? false
+    : text.split(".").every((item) => {
+        if (
+          parseInt(item) === NaN ||
+          item.split("").length > 3 ||
+          (item.split("").length === 3 && parseInt(item) < 100)
+        ) {
+          return false;
+        }
+
+        return parseInt(item) >= 0 && parseInt(item) <= 255;
+      });
+};
 
 module.exports = isValidIPv4;
